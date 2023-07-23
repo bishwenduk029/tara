59b09bf647bbb48c9b13fda3bbf2433cbcd59568
Kundu, Bishwendu <bishwenduk029@gmail.com>
2023-07-23 17:13:01 +0530

    Improve the project

--- backend/llm.py
+++ backend/llm.py
@@ -1,9 +1,12 @@
-from langchain.llms import GPT4All
+from langchain.llms import OpenAI
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 
 
 # llm = OpenAI(temperature=0, model_name="gpt-4")
 # Callbacks support token-wise streaming
-callbacks = [StreamingStdOutCallbackHandler()]
-llm = GPT4All(model="/Users/kundb/Library/CloudStorage/OneDrive-PegasystemsInc/backup/Downloads/wizardlm-13b-v1.1-superhot-8k.ggmlv3.q4_0.bin",
-              callbacks=callbacks, verbose=True)
+# callbacks = [StreamingStdOutCallbackHandler()]
+# llm = GPT4All(model="/Users/kundb/Library/CloudStorage/OneDrive-PegasystemsInc/backup/Downloads/wizardlm-13b-v1.1-superhot-8k.ggmlv3.q4_0.bin",
+#               callbacks=callbacks, verbose=True)
+
+llm = OpenAI(
+    temperature=0, model_name="gpt-4")
